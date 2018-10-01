5e557ce5e9411f8bc6a82c6c319b6ef3e7de5984
Eric Flores <ericjoseph.flores1@gmail.com>
2018-10-01 17:50:40 +0800

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -13,28 +13,17 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New user connected');
-    // socket.emit('newEmail', {
-    //     from: 'ther@example.com',
-    //     text: 'Hey, What is going on.',
-    //     createdAt: 123
-    // });
     socket.on('disconnect', (socket) => {
         console.log('Client disconnected');
     });
-    // socket.on('createEmail', (newEmail) => {
-    //     console.log('createEmail', newEmail);
-    // });
-    socket.emit('newMessage', {
-        from: 'EJFlow32',
-        text: 'WADDUPPPP',
-        createdAt: new Date().toString()
-    });
     socket.on('createMessage', (message) => {
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
         console.log(message);
     });
-    //newMessage Event
-    //from, text, createdAt
-    //create message on
 });
 
 server.listen(port, () => {
